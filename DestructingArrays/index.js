ac1092396f99259b976634731cdd42a51c8be0f0
nathan <75557717+nathanlao@users.noreply.github.com>
2022-12-26 15:36:51 -0800

    Destructing array

---
+++ DestructingArrays/index.js
@@ -0,0 +1,8 @@
+// let numberStr = ['one', 'two', 'three']
+
+// Destructing the array
+let [numberOne, numTwo, numThree] = ['one', 'two', 'three']
+
+// Can reassign the value
+numberOne = 'four'
+console.log(numberOne + numTwo + numThree)
